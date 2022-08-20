a9dfb290809c9425423c15156a69e4fa5a945850
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-20 17:51:22 +0530

    ADDED: Line Comparision equal or not

--- src/com/linecomparision/LineComparision.java
+++ src/com/linecomparision/LineComparision.java
@@ -6,9 +6,16 @@ public class LineComparision {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation");
 		
-		Line line = new Line(1.5, 1.4, 8.3, 9.1);
-		
-		System.out.println("length of line is "+line.length());
-		
+		Line line1 = new Line(1.8, 1.2, 8.1, 9.3);
+		Line line2 = new Line(8.1, 9.3, 1.8, 1.2);
+				
+		System.out.println("first line start point is ("+line1.x1+", "+line1.y1+"), end point is ("+line1.x2+", "+line1.y2+") and length of line is "+line1.length());
+		System.out.println("second line start point is ("+line2.x1+", "+line2.y1+"), end point is ("+line2.x2+", "+line2.y2+") and length of line is "+line2.length());
+
+		if (line1.length() == line2.length()) {
+			System.out.println("both lines are equal in length");
+		} else {
+			System.out.println("both line are not in length");
+		}
 	}
 }
