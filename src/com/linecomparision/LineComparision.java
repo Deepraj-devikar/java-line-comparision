e0ef1f2c52590e3497d75a8dcf9584d924d22a92
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-20 17:37:08 +0530

    ADDED: Line with functionality length

--- src/com/linecomparision/LineComparision.java
+++ src/com/linecomparision/LineComparision.java
@@ -1,8 +1,14 @@
 package com.linecomparision;
 
+import com.linecomparision.Line;
+
 public class LineComparision {
 	public static void main(String[] args) {
-		
 		System.out.println("Welcome to Line Comparison Computation");
+		
+		Line line = new Line(1.5, 1.4, 8.3, 9.1);
+		
+		System.out.println("length of line is "+line.length());
+		
 	}
 }
